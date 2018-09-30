380b12c9ce926f9cf96aed83bd4596fca81cc85f
Sean Eberhard <seaneberhard@gmail.com>
2018-09-30 16:46:34 +0100

    fix extension of output

--- latex2wp/main.py
+++ latex2wp/main.py
@@ -608,14 +608,14 @@ def main():
     import os
 
     parser = argparse.ArgumentParser(description='Convert LaTeX file to WordPress-ready HTML')
-    parser.add_argument('input_files', nargs='+', help='files to convert')
+    parser.add_argument('input_file', nargs='+', help='files to convert')
     parser.add_argument('--html', action='store_true', help='produce standard HTML instead of WordPress stuff')
     args = parser.parse_args()
 
     style.html = args.html
 
-    for input_file in args.input_files:
-        output_file = os.path.basename(input_file) + '.html'
+    for input_file in args.input_file:
+        output_file = os.path.splitext(os.path.basename(input_file))[0] + '.html'
         with open(input_file, 'r') as input_stream:
             latex = input_stream.read()
         html = convert_one(latex)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='latex2wp',
-    version='1.0.0',
+    version='1.0.1',
     packages=['latex2wp', 'latex2wp.test'],
     url='https://github.com/seaneberhard/latex2wp',
     license='GNU GPL3+',
